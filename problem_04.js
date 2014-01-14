fde8a791bbc109094d98b0027b01b8065685afd8
lobsterhands <denmanlp@gmail.com>
2014-01-14 17:03:01 -0600

    Update problem_04.js
    Some tweaks including printing out the multiples and also limiting [ while j >= i ] to remove duplication of effort in multiplying.

--- problem_04.js
+++ problem_04.js
@@ -1,14 +1,15 @@
 function palindrome(target) {
   var largestPalindrome = 0
   for (var i = target; i > 0; i--) {
-    for (j = target; j > 0; j--) {
+    for (j = target; j >= i ; j--) {
       var possPali = i * j;
       var paliTest = possPali.toString().split("").reverse("").join("")
       if (paliTest == possPali && paliTest > largestPalindrome) {
-        largestPalindrome = possPali;
+      largestPalindrome = possPali;
+      var multipliers = (" is the result of " + i + " * " + j + ".");
       }
       else if (i * target < largestPalindrome) {
-        return largestPalindrome;
+        return (largestPalindrome) + (multipliers);
       }
     }
   }
