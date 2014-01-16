51908a607c16e889876d7dc0540b779f90a58834
lobsterhands <denmanlp@gmail.com>
2014-01-15 21:55:33 -0600

    Create problem_05.js

---
+++ problem_05.js
@@ -0,0 +1,17 @@
+function smallestMultiple (goal) {
+  var list = [];
+  for (count = 1; count <= goal; count++ ) {
+    list.push(count)
+  }
+  for (i = goal*2; ; i += goal) {
+  for (j = 0; j <= list.length-1; j++) {
+    if (i % list[j] === 0 && j === list.length-1)
+      return i;
+    else if (i % list[j] === 0)
+      continue;
+    else
+      break;
+    }
+  }
+}
+console.log(smallestMultiple(20))
